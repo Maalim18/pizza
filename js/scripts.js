8af273d20ead5b4c73ffb56f52570da94484b6d4
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-04-25 19:51:37 +0300

    add final total prototype

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,12 @@ function Order(type, size, crust, topping) {
 Order.prototype.fullOrder = function () {
     return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
 };
+function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+}
+
+Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+};
